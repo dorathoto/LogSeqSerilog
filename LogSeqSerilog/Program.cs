ea88232c88a97baa13a65e6ec29185197752fec5
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-09-21 14:00:40 -0300

    Add project files.

---
+++ LogSeqSerilog/Program.cs
@@ -0,0 +1,41 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//var logger = new LoggerConfiguration()
+//    //.WriteTo.Console()
+//    .WriteTo.Seq("http://localhost:5341")
+//    .CreateLogger();
+//builder.Logging.AddSerilog(logger);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+
+
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
